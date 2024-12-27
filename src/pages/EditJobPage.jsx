61b9cba808fb039e6c3d8a977bd1b3c000ca6bf0
Shema-arsene <aniyonkurushema@gmail.com>
2024-12-27 08:34:16 +0200

    Troubleshoot 'Spinner' & 'Edit job function'

--- src/pages/EditJobPage.jsx
+++ src/pages/EditJobPage.jsx
@@ -41,7 +41,7 @@ const EditJobPage = ({ updateJobSubmit }) => {
       },
     }
 
-    updateJobSubmit(editJobSubmit)
+    updateJobSubmit(updatedJob)
 
     toast.success("Job listing updated successfully")
 
@@ -52,7 +52,7 @@ const EditJobPage = ({ updateJobSubmit }) => {
     <section className="bg-indigo-50">
       <div className="container m-auto max-w-2xl py-24">
         <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
-          <form onSubmit={formSubmit}>
+          <form onSubmit={editJobSubmit}>
             <h2 className="text-3xl text-center font-semibold mb-6">
               Update Job
             </h2>
