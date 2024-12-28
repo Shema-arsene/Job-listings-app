1960d65be6472e2e484229685546ba6320c949e0
Shema-arsene <aniyonkurushema@gmail.com>
2024-12-28 12:26:00 +0200

    Intergrated the filter buttons into the loading logic

--- src/Components/JobListings.jsx
+++ src/Components/JobListings.jsx
@@ -32,21 +32,6 @@ const JobListings = ({ isHome = false }) => {
 
   return (
     <section className="bg-blue-50 px-4 py-10">
-      <div className="flex justify-center space-x-4 mb-7">
-        {tabs.map((tab, index) => (
-          <button
-            key={index}
-            onClick={() => setActiveTab(tab)}
-            className={`py-2 px-4 rounded ${
-              activeTab === tab
-                ? "bg-indigo-500 border border-indigo-500 text-white"
-                : "border border-indigo-500 bg-blue-50 text-black"
-            }`}
-          >
-            {tab}
-          </button>
-        ))}
-      </div>
       <div className="container-xl lg:container m-auto">
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
           {isHome ? "Recent Jobs" : "Browse Jobs"}
@@ -54,10 +39,32 @@ const JobListings = ({ isHome = false }) => {
         {loading ? (
           <Spinner loading={loading} />
         ) : (
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {filteredJobs.map((job) => (
-              <JobListing key={job.id} job={job} />
-            ))}
+          <div>
+            <div
+              className={`${
+                isHome ? "hidden" : "flex"
+              } justify-center space-x-4 mb-7`}
+            >
+              {tabs.map((tab, index) => (
+                <button
+                  key={index}
+                  onClick={() => setActiveTab(tab)}
+                  className={`py-2 px-4 rounded ${
+                    activeTab === tab
+                      ? "bg-indigo-500 border border-indigo-500 text-white"
+                      : "border border-indigo-500 bg-blue-50 text-black"
+                  }`}
+                >
+                  {tab}
+                </button>
+              ))}
+            </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {filteredJobs.map((job) => (
+                <JobListing key={job.id} job={job} />
+              ))}
+            </div>
           </div>
         )}
       </div>
